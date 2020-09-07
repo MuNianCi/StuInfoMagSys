da225c92988582be697f4c0eacae5cd494f22ffd
MuNianCi <kadingche211@163.com>
2020-09-07 19:30:40 +0800

    commit by MuNianCi on 20200907

---
+++ admin/admin.c
@@ -0,0 +1,10 @@
+#include "admin.h"
+
+int  admin_login(){
+	struct  admin   adm;
+	printf("Welcome  Login  Stu  Sys\n");
+	printf("Please  Input  Your  Id\n");
+	scanf("%s", admin.id);
+	printf("Please  Input  Your  name");
+	scanf("%s", admin.name);
+}
