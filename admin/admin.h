e42855b4c3944b6a43908cca9a64aec3c51a77f8
MuNianCi <kadingche211@163.com>
2020-09-07 19:25:47 +0800

    commit by MuNC  on 20200907

---
+++ admin/admin.h
@@ -0,0 +1,17 @@
+#ifndef  _ADMIN_H_
+#define  _ADMIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct  admin{
+	int  id;
+	char name[64];
+	char pwd[32];
+	char rsv[1024];
+}
+
+#endif
+
