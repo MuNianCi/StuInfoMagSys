9d2727c84046906e63c32331ddea1115dbae3af8
inituu <56549122+inituu@users.noreply.github.com>
2020-09-08 22:02:53 +0800

    Update admin.c
    添加文件inituu

--- admin/admin.c
+++ admin/admin.c
@@ -2,12 +2,17 @@
 
 int  admin_login(){
 	struct  admin   adm;
-
+        int  fd;
+	fd = open("./config.ini", 0600);
+	if(fd < 0){
+	    perror("open file err\n");
+	    exit(-1);
+	}
 	printf("Please  Input  Your  Id\n");
 	scanf("%4d", &adm.id);
 	printf("Please  Input  Your  name\n");
 	scanf("%s", adm.name);
-
+        
 	printf("Input info adm.id=[%d]\nadm.name=[%s]\n",adm.id,adm.name );
 
 }
